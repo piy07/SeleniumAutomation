f9c3c55460bd189df2f72fb69eafea0c18ba9dbf
Piyush Verma <35527289+piy07@users.noreply.github.com>
2018-12-13 20:10:10 +0530

    Add .gitignore and .gitattributes.
    First Commit

---
+++ UnitTestProject1/FirstTestCase.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class FirstTestCase
+    {
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            IWebDriver driver = new ChromeDriver();
+
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("https://www.wikipedia.org/");
+            List<String> textofanchors = new List<string>();
+            ReadOnlyCollection<IWebElement> anchorList = driver.FindElements(By.TagName("a"));
+            foreach(IWebElement anchor in anchorList)
+            {
+                    if(anchor.Text.Length>0)
+                {
+                    if (anchor.Text.Contains("English"))
+                    {
+                        textofanchors.Add(anchor.Text);
+                        anchor.Click();
+                        break;
+                    }
+                }
+               
+            }
+            //driver.FindElement(By.Id("searchInput")).SendKeys("google");
+            //driver.FindElement(By.ClassName("svg-search-icon")).Click();
+            //string abc = driver.FindElement(By.Id("firstHeading")).Text.ToString();
+
+            driver.Close();
+            //driver.Quit();
+        }
+    }
+}
---
+++ UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3ccbcc26-8c22-46df-a66f-3ee50c71d7ab")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
